4ee95579fcd55eab2f78c7a3e7816afa619ce910
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-25 16:11:51 +0700

    Add relationships

--- app/ArticleComment.php
+++ app/ArticleComment.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Article;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ArticleComment extends Model
 {
-    //
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
 }
